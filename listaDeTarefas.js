ba4d9ebb20e98c0acd45166068c8e57203cb5b0d
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-15 13:35:29 -0300

    JavaScript IIFE

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,43 +1,46 @@
-const novaTarefa = document.querySelector('[data-form-button]');
+(() => {
 
-const criarTarefa = (event) => {
+    const novaTarefa = document.querySelector('[data-form-button]');
     
-    event.preventDefault();
-    const listaDeTarefas = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-    
-    const conteudo = `
-    <p class="content">${valor}<p/>
-    `;
-    
-    listaDeTarefas.appendChild(tarefa);
-    tarefa.innerHTML = conteudo;
-    tarefa.appendChild(BotaoConcluir());
-
-    input.value = '';
-}
-
-novaTarefa.addEventListener('click', criarTarefa);
-
-const BotaoConcluir = () => {
-    const botaoConcluir = document.createElement('button');
-    botaoConcluir.classList.add('check-button');
-    botaoConcluir.innerHTML = 'Concluir';
-    botaoConcluir.addEventListener('click', (event) => {
+    const criarTarefa = (event) => {
+        
         event.preventDefault();
-        concluirTarefa(event);
-    })
-    return botaoConcluir;
+        const listaDeTarefas = document.querySelector('[data-list]');
+        const input = document.querySelector('[data-form-input]');
+        const valor = input.value;
+        
+        const tarefa = document.createElement('li');
+        tarefa.classList.add('task');
+        
+        const conteudo = `
+        <p class="content">${valor}<p/>
+        `;
+        
+        listaDeTarefas.appendChild(tarefa);
+        tarefa.innerHTML = conteudo;
+        tarefa.appendChild(BotaoConcluir());
+        
+        input.value = '';
+    }
+    
+    novaTarefa.addEventListener('click', criarTarefa);
+    
+    const BotaoConcluir = () => {
+        const botaoConcluir = document.createElement('button');
+        botaoConcluir.classList.add('check-button');
+        botaoConcluir.innerHTML = 'Concluir';
+        botaoConcluir.addEventListener('click', (event) => {
+            event.preventDefault();
+            concluirTarefa(event);
+        })
+        return botaoConcluir;
 }
 
 const concluirTarefa = (event) => {
     const botaoConcluir = event.target;
-
+    
     const tarefaCompleta = botaoConcluir.parentElement;
     tarefaCompleta.classList.toggle('done');
 }
+})();
 
