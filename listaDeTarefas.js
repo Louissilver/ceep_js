1f3bc5ff477afc470d431d3aac23bdae66673b08
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-15 13:09:11 -0300

    Adicionando tarefa e criando elemento

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -3,11 +3,23 @@ const novaTarefa = document.querySelector('[data-form-button]');
 const criarTarefa = (event) => {
     
     event.preventDefault();
+    const listaDeTarefas = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
-    console.log(valor);
+    
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    
+    const conteudo = `
+    <p class="content">${valor}<p/>
+    `;
+    
+    listaDeTarefas.appendChild(tarefa);
+    tarefa.innerHTML = conteudo;
+    
     input.value = "";
 }
+
 
 
 novaTarefa.addEventListener('click', criarTarefa);
